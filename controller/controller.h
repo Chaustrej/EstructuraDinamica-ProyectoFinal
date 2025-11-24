0fd8f610813525c34db78735839036a19f5a273a
Chaustrej <131916274+Chaustrej@users.noreply.github.com>
2025-11-23 21:16:29 -0400

    Ahi vamos

---
+++ controller/controller.h
@@ -0,0 +1,8 @@
+#ifndef CONTROLLER_H
+#define CONTROLLER_H
+
+// Funciones que la View llamará cuando ocurra un evento
+void controller_eventoCargarDatos();
+void controller_eventoAsignarRey();
+
+#endif
