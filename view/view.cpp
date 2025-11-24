763bb49c8c9462786c38d00f5e5d4df73b79c0de
luiso4 <luisejandrobm@gmail.com>
2025-11-24 16:38:32 -0400

    view.cpp listo

--- view/view.cpp
+++ view/view.cpp
@@ -17,3 +17,55 @@ void helper_imprimirNodo(Persona* nodo, int nivel) {
     helper_imprimirNodo(nodo->hijoMayor, nivel+1);
     helper_imprimirNodo(nodo->hijoMenor, nivel+1);
 }
+// --- FUNCIONES PÚBLICAS ---
+
+void view_mostrarMensaje(string msg) {
+    cout << "\n[SISTEMA]: " << msg << endl;
+}
+
+void view_anunciarRey(Persona* rey) {
+    cout << "--------------------------------" << endl;
+    if (rey) cout << "HABEMUS REX: " << rey->name << " " << rey->lastName << endl;
+    else cout << "EL LINAJE HA TERMINADO." << endl;
+    cout << "--------------------------------" << endl;
+}
+
+void view_mostrarArbol(Persona* raiz) {
+    cout << "\n=== ARBOL GENEALOGICO ===" << endl;
+    if (!raiz) cout << "(Vacio)" << endl;
+    else helper_imprimirNodo(raiz, 0);
+    cout << "=========================\n" << endl;
+}
+// BUCLE PRINCIPAL (El usuario vive aquí)
+void view_iniciarSistema() {
+    int opcion = 0;
+    bool ejecutar = true;
+
+    while (ejecutar) {
+        cout << "\n1. Cargar Datos" << endl;
+        cout << "2. Asignar Rey" << endl;
+        cout << "3. Salir" << endl;
+        cout << ">> ";
+        
+        if (!(cin >> opcion)) {
+            cin.clear(); cin.ignore(1000, '\n'); opcion = 0;
+        }
+
+        switch (opcion) {
+            case 1:
+                // VIEW LLAMA A CONTROLLER
+                controller_eventoCargarDatos();
+                break;
+            case 2:
+                // VIEW LLAMA A CONTROLLER
+                controller_eventoAsignarRey();
+                break;
+            case 3:
+                ejecutar = false;
+                view_mostrarMensaje("Saliendo...");
+                break;
+            default:
+                view_mostrarMensaje("Opcion invalida.");
+        }
+    }
+}
